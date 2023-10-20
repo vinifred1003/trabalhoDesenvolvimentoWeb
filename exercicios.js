ffd8469c7120ca613bd570fe222b4494a411dda6
vinifred1003 <viniciusfrederico1003@gmail.com>
2023-10-20 18:26:32 -0300

    final

--- exercicios.js
+++ exercicios.js
@@ -1,25 +1,26 @@
+
 var saida = document.getElementById("codigo");
 
 function divisores(num) {
   num = +num;
-  let divisor=0;
+  let divisor = 0;
   for (let i = 0; i < num; i++) {
     if (num % i == 0) {
       divisor += i;
     }
   }
-  saida.value=divisor.toString()
+  saida.value = divisor.toString()
 };
 
 function distanciaEuclidiana(cordenadas) {
-  lista_de_valores=cordenadas.split(" ")
-  for(let i=0;i<lista_de_valores.length;i++){
-    lista_de_valores[i]= +lista_de_valores[i]
-  } 
-  x1=lista_de_valores[0]
-  x2=lista_de_valores[1]
-  y1=lista_de_valores[2]
-  y2=lista_de_valores[3]
+  lista_de_valores = cordenadas.split(" ")
+  for (let i = 0; i < lista_de_valores.length; i++) {
+    lista_de_valores[i] = +lista_de_valores[i]
+  }
+  x1 = lista_de_valores[0]
+  x2 = lista_de_valores[1]
+  y1 = lista_de_valores[2]
+  y2 = lista_de_valores[3]
   let result = ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2))
   result = Math.sqrt(result)
   saida.value = result.toFixed(2).toString()
@@ -41,7 +42,7 @@ function contadorPalavras(frase) {
       }
     }
   }
-  saida.value=contadorDePalavras;
+  saida.value = JSON.stringify(contadorDePalavras);
 }
 
 
@@ -55,58 +56,56 @@ function identificadorDeMatriz(matriz) {
     matrizCortada = matriz.split(';')
     numColunas = matrizCortada[0].length
   }
-  console.log(`Essa é uma matriz de dimensão ${numLinhas} X ${numColunas}`);
+  saida.value = `Essa é uma matriz de dimensão ${numLinhas} X ${numColunas}`;
 }
 
 function fibonaci(quant) {
-  quant= +quant
+  let sequenciaFib = "0 , 1";
+  quant = +quant;
   let x, z = 0
   let y = 1
-  if (quant < 2) {
-    console.log(0);
-  }
-  else if (quant == 2) {
-    console.log("0 \n1");
-  }
+  if (quant < 2) { saida.value = "0"; }
+  else if (quant == 2) { saida.value = "0 , 1"; }
   else {
-    console.log(" 0 \n1");
-    for (let i = 0; i < quant; i++) {
+    for (let i = 2; i < quant; i++) {
       x = y + z
       z = y
       y = x
-      saida.value= x.toString();
+      sequenciaFib += " , ".concat(x.toString())
     }
+    saida.value = sequenciaFib
   }
 }
 
-let  opcaoSelecionada
-document.getElementById("verificar").addEventListener("click", function () {
+let opcaoSelecionada
+
+document.getElementById("entrada").addEventListener("click", function () {
   var opcoes = document.getElementsByName("radio-btn");
 
   for (var i = 0; i < opcoes.length; i++) {
-      if (opcoes[i].checked) {
-          opcaoSelecionada = opcoes[i].id;
-          break; 
-      }
+    if (opcoes[i].checked) {
+      opcaoSelecionada = opcoes[i].id;
+      break;
     }
+  }
 })
 
-function execute(opcaoSelecionada){
+function execute(opcaoSelecionada) {
   var campoTexto = document.getElementById("entrada");
   var valorInserido = campoTexto.value;
-  if (opcaoSelecionada=="radio1"){
+  if (opcaoSelecionada == "radio1") {
     divisores(valorInserido)
   }
-  else if(opcaoSelecionada=="radio2"){
+  else if (opcaoSelecionada == "radio2") {
     distanciaEuclidiana(valorInserido)
   }
-  else if(opcaoSelecionada=="radio3"){
+  else if (opcaoSelecionada == "radio3") {
     contadorPalavras(valorInserido)
   }
-  else if(opcaoSelecionada=="radio4"){
+  else if (opcaoSelecionada == "radio4") {
     identificadorDeMatriz(valorInserido)
   }
-  else if(opcaoSelecionada=="radio      5"){
+  else if (opcaoSelecionada == "radio5") {
     fibonaci(valorInserido)
   }
 }
